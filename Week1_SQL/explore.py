adb72c688b01fc48bf4bd22d47d1569b0d611943
0xBlue2 <42499655+0xBlue2@users.noreply.github.com>
2026-05-13 08:50:54 -0400

    reorganized week1 file into functions, added working pandas version

--- Week1_SQL/explore.py
+++ Week1_SQL/explore.py
@@ -1,43 +1,30 @@
 import duckdb
-from Levenshtein import distance
 from pathlib import Path
-
-# return the possible matches for a string, using values from a list
-# we match by seeing if levenshtein_distance(comparison, list[i]) is less than or equal to threshold
-def return_possible_matches(comparison_value: str, list: list[str], threshold: int = 4) -> list[str]:
-    matches: list[str] = []
-
-    for val in list:
-        if distance(comparison_value, val) <= threshold:
-            matches.append(val)
-
-    return matches
+import pandas as pd
 
 WEEK_ROOT = Path(__file__).parent
 path = WEEK_ROOT / "data/Direct_Contributions_and_JFC_Distributions.csv"
-query = f"""
-    SELECT contrib as donor, count(*) as donations
+# SBF_CONTRIB_ID = "U0000004705"
+
+def read_with_duckdb():
+    query = f"""
+    SELECT ANY_VALUE(contrib) as donor, contribid, SUM(CAST(REPLACE(REPLACE(amount, '$', ''), ',', '') AS FLOAT)) as total_amount
     FROM '{path.absolute()}'
-    GROUP BY donor
-    ORDER BY donations desc
-"""
+    GROUP BY contribid
+    ORDER BY total_amount DESC
+    """
+    results = duckdb.query(query).to_df()
+    print(results)
 
+def read_with_pandas():
+    df = pd.read_csv(path)
+    df = df[['amount', 'contribid', 'contrib']]
+    df['amount'] = df['amount'].replace('[\\$,]', '', regex=True).astype(float)
 
+    unique_df = df.groupby('contribid', as_index=False).agg(
+        total=('amount', 'sum'),
+        name=('contrib', 'first')
+    ).sort_values('total', ascending=False)
+    print(unique_df)
 
-results = duckdb.query(query).to_df()
-global matches
-matches: list[list[str]] = []
-
-global total_donations
-total_donations: list[list[int]] = []
-for donor in results.donor:
-    # values of 6 or lower seem to work for nishad singh, but we miss out on SBF matches that could be caught with threshold of 11
-    # todo - write version that accounts for swapped first/last names
-    found_matches = return_possible_matches(donor, results.donor.tolist(), 6)
-    matches.append(found_matches)
-    total_donations.append([results.donations[results.donor == match].tolist() for match in found_matches])
-
-results["matches"] = matches
-results["total_donations"] = total_donations
-
-print(results.head(1))
+read_with_pandas()
